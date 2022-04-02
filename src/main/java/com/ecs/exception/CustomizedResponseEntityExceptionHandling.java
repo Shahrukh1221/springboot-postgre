d88ccf2da02e43cc09d97fc3dc271c3755d9a431
Mohd Shahrukh <mohdshahrukh8499@gmail.com>
2022-04-02 16:28:18 +0530

    spring boot postgresql heroku deployment

---
+++ src/main/java/com/ecs/exception/CustomizedResponseEntityExceptionHandling.java
@@ -0,0 +1,47 @@
+package com.ecs.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandling 
+extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException
+		(Exception ex, WebRequest request){
+			
+		ExceptionResponse exceptionResponse=new ExceptionResponse(new Date(),ex.getMessage(),request.getDescription(false));
+		return new ResponseEntity(exceptionResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotException
+		(UserNotFoundException ex, WebRequest request){
+			
+		ExceptionResponse exceptionResponse=new ExceptionResponse(new Date(),ex.getMessage(),request.getDescription(false));
+		return new ResponseEntity(exceptionResponse,HttpStatus.NOT_FOUND);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		String message=ex.getBindingResult().toString();
+		message =message.substring(message.lastIndexOf(';')+18);
+		ExceptionResponse exceptionResponse=new ExceptionResponse(new Date(),"Validation Failed",message);
+		return new ResponseEntity(exceptionResponse,HttpStatus.BAD_REQUEST);
+	}
+
+	
+}
